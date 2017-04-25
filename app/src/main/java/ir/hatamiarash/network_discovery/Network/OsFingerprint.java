508ddacd374630cc6fb56eecb29213ac6a6eeaf7
Arash Hatami <hatamiarash7@gmail.com>
2017-04-26 02:04:20 +0430

    Upload Application
    first release

---
+++ app/src/main/java/ir/hatamiarash/network_discovery/Network/OsFingerprint.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2017 - All Rights Reserved - Arash Hatami
+ */
+
+package ir.hatamiarash.network_discovery.Network;
+
+public class OsFingerprint {
+
+    // private final static String TAG = "OsFingerprint";
+
+    public static String finger(long[] longArray) {
+        return null;
+    }
+}
---
+++ app/src/main/java/ir/hatamiarash/network_discovery/Utils/Help.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2017 - All Rights Reserved - Arash Hatami
+ */
+
+package ir.hatamiarash.network_discovery.Utils;
+
+import ir.hatamiarash.network_discovery.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Help extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
+    }
+}
